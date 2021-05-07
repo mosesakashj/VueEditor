f0c32d8e603e865e76fde5621c3d6fa7f601d02b
Akash <mosesakash.j@gmail.com>
2021-05-07 16:40:24 +0530

    editor improved

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,15 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      Text Editor
-      <v-spacer></v-spacer>
-
-      <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-main>
-      <HelloWorld class="pa-10"/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
   data: () => ({
     //
   }),
---
+++ src/components/vue-file-toolbar-menu.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+      <template v-for="(i, index) in content" >
+        <v-menu offset-y open-on-hover :key="index" v-if="i.menu">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn v-bind="attrs" v-on="on" small tile><v-icon>{{i.icon}}</v-icon> </v-btn>
+          </template>
+          <v-list height="200">
+            <v-list-item v-for="(item, i) in i.menu" :key="i">
+              <v-list-item-title @click="item.click">{{ item.text }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+        <v-btn @click="i.click()" :key="index" v-else-if="i.icon" tile small><v-icon>{{i.icon}}</v-icon> </v-btn>
+        <v-btn @click="i.click()" :key="index" v-else-if="i.html" v-html="i.html" tile small></v-btn>
+      </template>
+  </div>
+</template>
+
+<script>
+export default {
+    props: [ 'content' ]
+}
+</script>
+
+<style>
+
+</style>
