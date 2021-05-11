ab9e179f1298c2754e11373d0128b04fabc59c73
Akash <mosesakash.j@gmail.com>
2021-05-11 11:45:08 +0530

    font color initial commit implemented

--- src/components/vue-file-toolbar-menu.vue
+++ src/components/vue-file-toolbar-menu.vue
@@ -3,23 +3,34 @@
       <template v-for="(i, index) in content" >
         <v-menu offset-y open-on-hover :key="index" v-if="i.menu">
           <template v-slot:activator="{ on, attrs }">
-            <v-btn v-bind="attrs" v-on="on" small tile ><v-icon>{{i.icon}}</v-icon> </v-btn>
+            <v-btn v-bind="attrs" v-on="on" small tile :disabled="i.disabled"><v-icon>{{i.icon}}</v-icon> </v-btn>
           </template>
-          <v-list height="200">
+          <v-list max-height="200">
             <v-list-item v-for="(item, i) in i.menu" :key="i">
               <v-list-item-title @click="item.click">{{ item.text }}</v-list-item-title>
             </v-list-item>
           </v-list>
         </v-menu>
-        <v-btn @click="i.click()" :key="index" v-else-if="i.icon" tile small :color="i.active ? 'primary' : ''"><v-icon>{{i.icon}}</v-icon> </v-btn>
-        <v-btn @click="i.click()" :key="index" v-else-if="i.html" v-html="i.html" tile small :color="i.active ? 'primary' : ''"></v-btn>
+        <v-tooltip bottom  v-else-if="i.icon"  :key="index">
+          <template v-slot:activator="{ on, attrs }">
+          <v-btn @click="i.click()" tile small :color="i.active ? 'primary' : ''" :disabled="i.disabled" v-bind="attrs" v-on="on"><v-icon>{{i.icon}}</v-icon> </v-btn>
+          </template>
+          <span>{{i.title}}</span>
+        </v-tooltip>
+        <v-btn @click="i.click()" :key="index" v-else-if="i.html" v-html="i.html" tile small :color="i.active ? 'primary' : ''" :disabled="i.disabled"></v-btn>
+        <input class="color-apply" :disabled="i.disabled" :key="index" type="color" @change="i.update_color(selectedColor)" v-model="selectedColor" v-if="i.is === 'button-color'">
       </template>
   </div>
 </template>
 
 <script>
 export default {
-    props: [ 'content' ]
+    props: [ 'content' ],
+    data () {
+      return {
+        selectedColor: ''
+      }
+    }
 }
 </script>
 
---
+++ src/views/Loader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <v-img src="https://flevix.com/wp-content/uploads/2019/12/Quarter-Circle-Loading-Image-1.gif" :height="height" :width="width"></v-img>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      height: window.innerHeight,
+      width: window.innerWidth
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
